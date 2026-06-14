fa4044ed2b76273dffc928b851b0f1bb84ed1394
UV Interactives <huntingblu@gmail.com>
2026-06-14 07:32:58 +0530

    bar update

--- src/components/island/hud/StatsHUD.jsx
+++ src/components/island/hud/StatsHUD.jsx
@@ -10,7 +10,7 @@ const STUDIO_STATS = [
 const StatsHUD = ({ isDark }) => (
   <div style={{
     position: 'absolute',
-    top: '150px',
+    top: '96px',
     right:'20px',
     zIndex: 20,
     pointerEvents: 'none',
--- src/components/island/hud/TitleHUD.jsx
+++ src/components/island/hud/TitleHUD.jsx
@@ -4,7 +4,7 @@
 const TitleHUD = ({ isDark }) => (
   <div style={{
     position: 'absolute',
-    top: '150px',
+    top: '96px',
     left:'20px',
     zIndex: 20,
     pointerEvents: 'none'
